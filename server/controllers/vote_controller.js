f16b87038fa61519f5cb82eb20724113977a36ab
Brachi20 <b0583919569@gmail.com>
2025-01-12 20:46:12 +0200

    Implemented vote saving logic in vote_controller.js, Updated vote schema in vote_model.js

--- server/controllers/vote_controller.js
+++ server/controllers/vote_controller.js
@@ -1,11 +1,46 @@
 import BaseController from './base_controller.js';
 import Vote from '../models/vote_model.js';
+import Scenario from "../models/scenario_model";
 
 class VoteController extends BaseController {
     constructor() {
         super(Vote); 
     }
-
+    
+    async create(req, res){
+        const { room_id } = req.params;
+        const { scenario_id, answer, comment, user_id } = req.body;
  
+    try {
+        if (!scenario_id || answer === undefined || !user_id) {
+          return res.status(400).json({ status: false, message: "Missing required fields" });
+        }
+    
+        // check scenario corect
+        const scenario = await Scenario.findById(scenario_id);
+        if (!scenario) {
+          return res.status(404).json({ status: false, message: "Scenario not found" });
+        }
+    
+        // create and save new vote
+        const newVote = new Vote({
+          room_id,
+          scenario_id,
+          user_id,
+          answer,
+          comment: comment || null,
+        });
+    
+        await newVote.save();
+    
+        // TODO: עדכון סטטיסטיקות (כאן אפשר להוסיף לוגיקה לחישוב סטטוס הצבעות)
+    
+        res.status(201).json({ status: true, message: "Answer submitted successfully" });
+      } catch (error) {
+        console.error("Error submitting answer:", error);
+        res.status(500).json({ status: false, message: "Internal server error" });
+      }
+    };
+    
 }
 export default new VoteController();
--- server/routes/vote_route.js
+++ server/routes/vote_route.js
@@ -2,14 +2,16 @@ import express from 'express';
 const router = express.Router();
 import voteController from '../controllers/vote_controller.js';
 
-
-router.post('/', (req, res) => voteController.create(req, res));
+//TODO: update vote page path
+router.post('/rooms/:room_id/answers', (req, res) => voteController.create(req, res));
 router.get('/', (req, res) => voteController.getAll(req, res));
 router.get('/:id', (req, res) => voteController.getById(req, res));
 router.put('/:id', (req, res) => voteController.update(req, res));
 router.delete('/:id', (req, res) => voteController.delete(req, res));
-
-
-
+router.post("/rooms/:room_id/answers", voteController.submitAnswer);
 
 export default router;
+
+
+
+
