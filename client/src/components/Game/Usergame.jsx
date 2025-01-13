faa1d73e4b357b6628eb6bad64928252076fb19f
Ziv Francis <zivfr@mta.ac.il>
2025-01-13 23:47:01 +0200

    fixed css bug added disable after clicking

--- client/src/components/Game/Usergame.jsx
+++ client/src/components/Game/Usergame.jsx
@@ -1,12 +1,17 @@
+import { useState } from 'react';
 import '../Usergame.css';
 
 const UserGame = () => {
+  const [showButtons, setShowButtons] = useState(true);
+
   const handleCorrectClick = () => {
     console.log('תקין');
+    setShowButtons(false); 
   };
 
   const handleIncorrectClick = () => {
     console.log('לא תקין');
+    setShowButtons(false); 
   };
 
   const handleAddAnonymousSituation = () => {
@@ -14,18 +19,41 @@ const UserGame = () => {
   };
 
   return (
-    <div className="user-game-container">
-      <div className="button-container">
-        <button className="button correct-button" onClick={handleCorrectClick}>
-          תקין
-        </button>
-        <button className="button incorrect-button" onClick={handleIncorrectClick}>
-          לא תקין
+    <div className="phone-frame">
+      <div className="user-game-container">
+        {showButtons ? (
+          <div className="button-container">
+            {}
+            <button
+              className="usergame-button correct-button"
+              onClick={handleCorrectClick}
+            >
+              תקין
+            </button>
+            
+            {}
+            <button
+              className="usergame-button incorrect-button"
+              onClick={handleIncorrectClick}
+            >
+              לא תקין
+            </button>
+          </div>
+        ) : (
+            <div className="waiting-text-container">
+            <p className="waiting-text">הצבעתך נקלטה בהצלחה</p>
+            <p className="waiting-text">המתן לתוצאות</p>
+            </div>
+        )}
+        
+        {}
+        <button
+          className="usergame-button add-situation-button"
+          onClick={handleAddAnonymousSituation}
+        >
+          הוסף סיטואציה אנונימית
         </button>
       </div>
-      <button className="button add-situation-button" onClick={handleAddAnonymousSituation}>
-        הוסף סיטואציה אנונימית
-      </button>
     </div>
   );
 };
